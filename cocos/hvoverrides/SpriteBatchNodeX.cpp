ab4dd866fa28262ba643ebe6ee8585faa416ff86
joelmay <jamay@connectedpixel.com>
2015-07-11 03:55:36 -0500

    removed commetned-out code.

--- cocos/hvoverrides/SpriteBatchNodeX.cpp
+++ cocos/hvoverrides/SpriteBatchNodeX.cpp
@@ -51,31 +51,9 @@ void hv::SpriteBatchNodeX::draw(
     }
     
     _customCommand.init(_globalZOrder, transform, flags);
-    //_customCommand.func = CC_CALLBACK_0(executeRenderCommand, this, transform, transformUpdated);
     _customCommand.func = CC_CALLBACK_0(hv::SpriteBatchNodeX::executeRenderCommand, this, transform, flags);
-    //_customCommand.func = CC_CALLBACK_0(hv::SpriteBatchNodeX::executeRenderCommand, this, transform, transformUpdated);
     
     renderer->addCommand(&_customCommand);
-    
-    
-//    
-////    SpriteBatchNode::draw(renderer, transform, flags);
-////    return;
-//
-//    // Don't do calculate the culling if the transform was not updated
-//    bool transformUpdated = flags & FLAGS_TRANSFORM_DIRTY;
-//#if CC_USE_CULLING
-//    bool bInsideBounds = transformUpdated ? renderer->checkVisibility(transform, _contentSize) : _insideBounds;
-//    
-//    if(bInsideBounds)
-//#endif
-//    {
-//        _customCommand.init(_globalZOrder, transform, flags);
-//        //_customCommand.func = CC_CALLBACK_0(executeRenderCommand, this, transform, transformUpdated);
-//        _customCommand.func = CC_CALLBACK_0(hv::SpriteBatchNodeX::executeRenderCommand, this, transform, transformUpdated);
-//        
-//        renderer->addCommand(&_customCommand);
-//    }
 }
 
 
@@ -89,54 +67,14 @@ void hv::SpriteBatchNodeX::executeRenderCommand(const cocos2d::Mat4& transform, 
     programState->apply(transform);
     
     
-//    auto glProgramState = cocos2d::GLProgramState::getOrCreateWithGLProgram(program);
-    //ball->setGLProgramState(glProgramState);
-    
     auto textureID = _textureAtlas->getTexture()->getName();
-//    glProgramState->setUniformTexture("u_texture"         , textureID);
-//    glProgramState->setUniformMat4   ("ColorMatrix"       , _colorMatrix);
-    
-    //glProgramState->updateUniformsAndAttributes();
- 
-    
-    //program->setUniformsForBuiltins(_mv);
-//    glProgramState->applyUniforms();
-    
     
     cocos2d::GL::bindTexture2D(textureID);
     cocos2d::GL::blendFunc(_blendFunc.src, _blendFunc.dst);
-    //cocos2d::GL::blendFunc(_blendType.src, _blendType.dst);
     
    
-    //program->use();
-    
     // Draw
     _textureAtlas->drawQuads();
-    
-    
-//    
-//    
-//    // Optimization: Fast Dispatch
-//    if( _textureAtlas == NULL || (_batchNodes.size() == 1 && _textureAtlas->getTotalQuads() == 0) )
-//    {
-//        return;
-//    }
-//    
-//    auto glprogram = getGLProgram();
-//    glprogram->use();
-//    cocos2d::GL::blendFunc( _blendFunc.src, _blendFunc.dst );
-//    
-//    glprogram->setUniformsForBuiltins(transform);
-//    for(const auto &child: _children)
-//    {
-//        child->updateTransform();
-//    }
-//    
-//    
-//    for (const auto& batchNode:_batchNodes)
-//    {
-//        batchNode->getTextureAtlas()->drawQuads();
-//    }
 }
 
 
